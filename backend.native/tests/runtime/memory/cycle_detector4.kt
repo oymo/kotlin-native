112da99d006b935ddd18f4cedfdcd1d7ddc1c866
Alexander Shabalin <alexander.shabalin@jetbrains.com>
2020-07-08 11:02:18 +0300

    Add a negative test

--- backend.native/tests/runtime/memory/cycle_detector4.kt
+++ backend.native/tests/runtime/memory/cycle_detector4.kt
@@ -1,9 +1,13 @@
 import kotlin.native.concurrent.*
+import kotlin.native.internal.GC
 import kotlin.test.*
 
 class Holder(var other: Any?)
 
 fun main() {
-    val atomic = AtomicReference<Any?>(null)
-    atomic.value = Pair(atomic, Holder(atomic)).freeze()
+    val atomic1 = AtomicReference<Any?>(null)
+    val atomic2 = AtomicReference<Any?>(null)
+    atomic1.value = atomic2
+    val cycles = GC.detectCycles()!!
+    assertEquals(0, cycles.size)
 }
