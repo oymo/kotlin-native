dfa14166298cd3203d0060e4824c9c3ba640b505
Alexander Shabalin <alexander.shabalin@jetbrains.com>
2020-07-14 13:40:16 +0300

    Unify tests in one file

---
+++ backend.native/tests/runtime/memory/cycle_detector.kt
@@ -0,0 +1,60 @@
+import kotlin.native.concurrent.*
+import kotlin.native.internal.GC
+import kotlin.test.*
+
+class Holder(var other: Any?)
+
+@Test
+fun test1() {
+    val a = AtomicReference<Any?>(null)
+    val b = AtomicReference<Any?>(null)
+    a.value = b
+    b.value = a
+    val cycles = GC.detectCycles()!!
+    assertEquals(1, cycles.size)
+    val cycle = GC.findCycle(cycles[0])!!
+    assertEquals(2, cycle.size)
+    assertTrue(cycle.contains(a))
+    assertTrue(cycle.contains(b))
+    a.value = null
+}
+
+@Test
+fun test2() {
+    val array = arrayOf(AtomicReference<Any?>(null), AtomicReference<Any?>(null))
+    val obj1 = Holder(array).freeze()
+    array[0].value = obj1
+    val cycles = GC.detectCycles()!!
+    assertEquals(1, cycles.size)
+    assertTrue(arrayOf(obj1, array, array[0]).contentEquals(GC.findCycle(cycles[0])!!))
+    array[0].value = null
+}
+
+@Test
+fun test3() {
+    val a1 = FreezableAtomicReference<Any?>(null)
+    val head = Holder(null)
+    var current = head
+    repeat(30) {
+        val next = Holder(null)
+        current.other = next
+        current = next
+    }
+    a1.value = head
+    current.other = a1
+    current.freeze()
+    val cycles = GC.detectCycles()!!
+    assertEquals(1, cycles.size)
+    val cycle = GC.findCycle(cycles[0])!!
+    assertEquals(32, cycle.size)
+    a1.value = null
+}
+
+@Test
+fun test4() {
+    val atomic1 = AtomicReference<Any?>(null)
+    val atomic2 = AtomicReference<Any?>(null)
+    atomic1.value = atomic2
+    val cycles = GC.detectCycles()!!
+    assertEquals(0, cycles.size)
+}
